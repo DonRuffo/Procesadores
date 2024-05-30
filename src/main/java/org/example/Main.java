2141b3e440fa0f3337a3b4eb7df106c6b107b2fd
isaacquinapallo <isaac.quinapallo@epn.edu.ec>
2024-05-30 08:15:32 -0500

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,11 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import.Procesadores;
 public class Main {
     public static void main(String[] args) {
-        int nucleos;
-        Double consumo, velocidad;
-        String nombre;
+        Procesadores p1=new Procesadores();
 
-        public Procesadores(){
-        }
-        public Procesadores(int nucleos, Double consumo, Double velocidad, String nombre){
-
-        }
+        //hola//
     }
 }
---
+++ src/main/java/org/example/Procesadores.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Procesadores {
+    public Procesadores() {
+        int nucleos;
+        Double consumo, velocidad;
+        String nombre;
+
+        public Procesadores(int nucleos, Double consumo, Double velocidad, String nombre){
+    }
+
+    //metodos personalizados
+
+}
